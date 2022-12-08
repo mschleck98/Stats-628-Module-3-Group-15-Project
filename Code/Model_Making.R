346f697098f5a9a8765af6a49dcc133407346f00
mschleck98 <52256805+mschleck98@users.noreply.github.com>
2022-12-07 23:25:11 -0600

    Add files via upload

---
+++ Code/Model_Making.R
@@ -0,0 +1,67 @@
+library(ggplot2)
+library(survival)
+library(survminer)
+library(ranger)
+library(ggplot2)
+library(dplyr)
+library(ggfortify)
+
+
+
+
+dfClosed <- read.csv(file = 'data_model_closed.csv')
+dfOpen <- read.csv(file = 'data_model_open.csv')
+
+dfFull <- rbind(dfClosed,dfOpen)
+
+dfFull$time[is.na(dfFull$time)] <- 0
+dfFull <- dfFull[dfFull$time >0,]
+
+# vet <- mutate(dfFull, SEN = ifelse((sentiment_score < 0), "LT3", "OV3"),
+#               SEN = factor(SEN))
+# dfFull$is_open <- 1 - dfFull$is_open
+dfFull$food <- dfFull$food
+# full.cox <- coxph(Surv(time, is_open) ~ SEN + food + drink + ambience + price + service, data = vet)
+full.cox <- coxph(Surv(time, is_open) ~ food + drink + ambience + price + strata(service), data = dfFull)
+summary(full.cox)
+
+
+write.csv(dfFull, "full_model_cat.csv", row.names=FALSE)
+
+autoplot(survfit(full.cox))
+
+
+vet <- mutate(dfFull, AG = ifelse((stars_x < 3), "LT3", "OV3"),
+              AG = factor(AG))
+km_AG_fit <- survfit(Surv(time, is_open) ~ AG, data=vet)
+autoplot(km_AG_fit)
+
+temp <- survfit(full.cox)
+temp
+ggsurvplot(temp, color = "#2E9FDF",
+           ggtheme = theme_minimal(),data = dfFull)
+
+
+#Assumption Checking
+
+full.ph <- cox.zph(full.cox)
+full.ph
+
+ggcoxzph(full.ph)
+
+
+#Influential Observations
+ggcoxdiagnostics(full.cox, type = "dfbeta",linear.predictions = FALSE, ggtheme = theme_bw())[1]
+
+#Linear relationship of log(predictors)
+
+ggcoxfunctional(Surv(time, is_open) ~ log(food), data = dfFull)
+ggcoxfunctional(Surv(time, is_open) ~ log(drink), data = dfFull)
+ggcoxfunctional(Surv(time, is_open) ~ log(price), data = dfFull)
+ggcoxfunctional(Surv(time, is_open) ~ log(service), data = dfFull)
+ggcoxfunctional(Surv(time, is_open) ~ log(ambience), data = dfFull)
+
+
+
+
+
---
+++ Code/Time Column creation.R
@@ -0,0 +1,112 @@
+library(tidyverse)
+df <- read.csv(file = 'data_cleaned.csv')
+uniqueBar <- df[!duplicated(df$business_id),]
+iid <- df$business_id
+allR <- unique(iid)
+time <- c()
+modelDF <- uniqueBar[,c('business_id','name','is_open','stars_x')]
+
+for (bar in allR){
+  temp = df[df$business_id == bar,]
+  temp = temp[order(format(as.POSIXct(temp$date_x,format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')),]
+  first = format(as.POSIXct(temp$date_x[1],format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')
+  last = format(as.POSIXct(tail(temp$date_x,n=1),format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')
+  surv <- difftime(last,first,units="weeks")
+  print(surv)
+  if(is.na(surv) | (surv < 0)){
+    surv = 0
+  }
+  time <- c(time,surv)
+  print(bar)
+}
+big <- read.csv(file = 'final_grouped_business_id.csv')
+modelDF <- cbind(modelDF,time)
+modelDF <- merge(big,modelDF,by="business_id")
+
+modelDF$is_open <- 1 - modelDF$is_open
+
+modelDF$drink <- modelDF %>%
+  pull(drink) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+modelDF$food <- modelDF %>%
+  pull(food) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$ambience <- modelDF %>%
+  pull(ambience) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$service <- modelDF %>%
+  pull(service) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$price <- modelDF %>%
+  pull(price) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+write.csv(modelDF, "data_model_closed.csv", row.names=FALSE)
+
+# BIGTEMP <- df[df$name == "Mizu Asian Bistro",]
+
+
+
+
+
+
+df <- read.csv(file = 'data_cleaned_open.csv')
+uniqueBar <- df[!duplicated(df$business_id),]
+iid <- df$business_id
+allR <- unique(iid)
+time <- c()
+modelDF <- uniqueBar[,c('business_id','name','is_open','stars_x')]
+
+for (bar in allR){
+  temp = df[df$business_id == bar,]
+  temp = temp[order(format(as.POSIXct(temp$date_x,format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')),]
+  first = format(as.POSIXct(temp$date_x[1],format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')
+  last = format(as.POSIXct(tail(temp$date_x,n=1),format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')
+  surv <- difftime("2022-12-7",first,units="weeks")
+  print(surv)
+  if(is.na(surv) | (surv < 0)){
+    surv = 0
+  }
+  time <- c(time,surv)
+  print(bar)
+}
+big <- read.csv(file = 'final__grouped_business_id_open.csv')
+modelDF <- cbind(modelDF,time)
+modelDF <- merge(big,modelDF,by="business_id")
+
+modelDF$is_open <- 1 - modelDF$is_open
+
+modelDF$drink <- modelDF %>%
+  pull(drink) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+modelDF$food <- modelDF %>%
+  pull(food) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$ambience <- modelDF %>%
+  pull(ambience) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$service <- modelDF %>%
+  pull(service) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+
+modelDF$price <- modelDF %>%
+  pull(price) %>%
+  cut(breaks=c(-Inf,-.1,.05,.15,.4,Inf),
+      labels=c("1","2","3","4","5"))
+write.csv(modelDF, "data_model_open.csv", row.names=FALSE)
+
+
+
